decf327cbdf2556dd38cf126bd8eedcfcfbe73f0
Patrick Kuny <31052319+pmkuny@users.noreply.github.com>
2026-04-11 19:27:19 -0500

    Add shared Obsidian vault configuration for ~/Self

--- modules/home/obsidian.nix
+++ modules/home/obsidian.nix
@@ -2,6 +2,8 @@
 
 {
   programs.obsidian = {
+    cli.enable = true;
+
     enable = true;
     plugins = [
       "dataview"
@@ -13,6 +15,18 @@
       "vimMode" = true;
       "showLineNumber" = true;
       "readableLineLength" = true;
+      "newFileLocation" = "00 - Atomic";
+      "propertiesInDocument" = "source";
+      "livePreview" = true;
+    };
+    vaults = {
+      self = {
+        enable = true;
+        target = config.home.homeDirectory + "/Self";
+        settings = {
+          # Vault-specific settings can go here
+        };
+      };
     };
   };
 }
